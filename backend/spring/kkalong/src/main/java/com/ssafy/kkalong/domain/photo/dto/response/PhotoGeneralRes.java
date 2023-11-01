56c83b983ded182c500f755229649984a4de1693
강신욱 <bearforce42@daum.net>
2023-11-01 17:29:37 +0900

    S09P31C105-279 / init / Photo 도메인 관련 기능 초기화

---
+++ backend/spring/kkalong/src/main/java/com/ssafy/kkalong/domain/photo/dto/response/PhotoGeneralRes.java
@@ -0,0 +1,12 @@
+package com.ssafy.kkalong.domain.photo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PhotoGeneralRes {
+    private String msg;
+}
