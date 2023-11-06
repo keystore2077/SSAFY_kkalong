2c86fdc7b39fed27e00a3d33e7d4fe7785549827
hwnagsua <tsmich926@naver.com>
2023-11-06 17:28:58 +0900

    [FE]사진고르기 border추가

--- frontend/lib/avata/choicepicture.dart
+++ frontend/lib/avata/choicepicture.dart
@@ -227,28 +227,238 @@
 //   }
 // }
 
+// import 'package:flutter/material.dart';
+// import '../avata/avatacamera.dart';
+// import '../avata/wearcloth.dart';
+
+// class ChoicePicture extends StatefulWidget {
+//   const ChoicePicture({
+//     super.key,
+//     this.storage,
+//   });
+
+//   final storage;
+
+//   @override
+//   State<ChoicePicture> createState() => ChoicePictureState();
+// }
+
+// class ChoicePictureState extends State<ChoicePicture> {
+//   @override
+//   void initState() {
+//     super.initState();
+//     // 초기화 작업 수행
+//   }
+
+//   final savecloset = {
+//     "list": [
+//       {"image": "Assets/Image/TShirt.png", "name": "Top"},
+//       {"image": "Assets/Image/Pants.png", "name": "Pants"},
+//       {"image": "Assets/Image/Suit.png", "name": "Outer"},
+//       {"image": "Assets/Image/Skirt.png", "name": "Skirts"},
+//     ]
+//   };
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: const Color.fromARGB(255, 255, 255, 254),
+//       body: CustomScrollView(
+//         slivers: <Widget>[
+//           const SliverAppBar(
+//             backgroundColor: Color(0xFFF5BEB5),
+//             expandedHeight: 55.0,
+//             floating: true,
+//             pinned: true,
+//             title: Text(
+//               '깔롱의 사진',
+//               style: TextStyle(color: Colors.white),
+//             ),
+//             centerTitle: true,
+//             elevation: 0,
+//             leading: Text(''),
+//           ),
+//           SliverList(
+//             delegate: SliverChildListDelegate(
+//               [
+//                 AppBar(
+//                   toolbarHeight: 100,
+//                   centerTitle: true,
+//                   title: const Text(
+//                     '내 사진을 골라주세요!',
+//                     style: TextStyle(
+//                       fontSize: 25,
+//                       color: Color.fromARGB(255, 0, 0, 0),
+//                       fontWeight: FontWeight.w600,
+//                     ),
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//           SliverPadding(
+//             padding: const EdgeInsets.fromLTRB(20, 1, 20, 0),
+//             sliver: SliverGrid(
+//               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//                 crossAxisCount: 2,
+//                 crossAxisSpacing: 20.0,
+//                 mainAxisSpacing: 20.0,
+//               ),
+//               delegate: SliverChildBuilderDelegate(
+//                 (BuildContext context, int index) {
+//                   final item = savecloset['list']?[index];
+//                   if (item == null) {
+//                     return const SizedBox(); // 빈 위젯 반환
+//                   }
+//                   return GestureDetector(
+//                     onTap: () {},
+//                     child: Container(
+//                       color: const Color.fromARGB(255, 251, 235, 233),
+//                       child: Column(
+//                         mainAxisAlignment: MainAxisAlignment.center,
+//                         children: <Widget>[
+//                           Image.asset(
+//                             item["image"] ?? "Assets/Image/logo.png",
+//                             height: 125,
+//                             width: 180,
+//                           ),
+//                           Text(
+//                             item["name"] ?? "Unknown",
+//                             style: const TextStyle(
+//                               fontSize: 20,
+//                               fontWeight: FontWeight.w500,
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   );
+//                 },
+//                 childCount: savecloset['list']?.length ?? 0,
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//       bottomNavigationBar: Padding(
+//         padding: const EdgeInsets.fromLTRB(20, 2, 20, 2),
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 Expanded(
+//                   child: ElevatedButton(
+//                     onPressed: () {},
+//                     style: ElevatedButton.styleFrom(
+//                       backgroundColor: const Color(0xFFF5BEB5),
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(5.0),
+//                       ),
+//                     ),
+//                     child: const Text(
+//                       '다시선택',
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontWeight: FontWeight.w700,
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(width: 10),
+//                 Expanded(
+//                   child: ElevatedButton(
+//                     onPressed: () {
+//                       Navigator.push(
+//                         context,
+//                         MaterialPageRoute(
+//                             builder: (context) => const WearCloth()),
+//                       );
+//                     },
+//                     style: ElevatedButton.styleFrom(
+//                       backgroundColor: const Color(0xFFF5BEB5),
+//                       shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(5.0),
+//                       ),
+//                     ),
+//                     child: const Text(
+//                       '선택완료',
+//                       style: TextStyle(
+//                         color: Colors.white,
+//                         fontWeight: FontWeight.w700,
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             const SizedBox(height: 10),
+//             SizedBox(
+//                 width: 400,
+//                 child: ElevatedButton(
+//                   onPressed: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                           builder: (context) => const AvataPicture()),
+//                     );
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: const Color(0xFFF5BEB5),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(5.0),
+//                     ),
+//                   ),
+//                   child: const Text(
+//                     '사진찍기 ',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontWeight: FontWeight.w700,
+//                     ),
+//                   ),
+//                 )),
+//             const SizedBox(height: 10),
+//             SizedBox(
+//                 width: 400,
+//                 child: ElevatedButton(
+//                   onPressed: () {},
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: const Color(0xFFF5BEB5),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(5.0),
+//                     ),
+//                   ),
+//                   child: const Text(
+//                     '내 패션 찰칵 ',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontWeight: FontWeight.w700,
+//                     ),
+//                   ),
+//                 )),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import '../avata/avatacamera.dart';
 import '../avata/wearcloth.dart';
 
 class ChoicePicture extends StatefulWidget {
-  const ChoicePicture({
-    super.key,
-    this.storage,
-  });
-
   final storage;
 
+  const ChoicePicture({Key? key, this.storage}) : super(key: key);
+
   @override
-  State<ChoicePicture> createState() => ChoicePictureState();
+  _ChoicePictureState createState() => _ChoicePictureState();
 }
 
-class ChoicePictureState extends State<ChoicePicture> {
-  @override
-  void initState() {
-    super.initState();
-    // 초기화 작업 수행
-  }
+class _ChoicePictureState extends State<ChoicePicture> {
+  int? selectedIndex;
 
   final savecloset = {
     "list": [
@@ -260,24 +470,27 @@ class ChoicePictureState extends State<ChoicePicture> {
   };
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 254),
+      backgroundColor: const Color(0x0fffffff),
       body: CustomScrollView(
-        slivers: <Widget>[
+        slivers: [
+          // App Bar
           const SliverAppBar(
             backgroundColor: Color(0xFFF5BEB5),
             expandedHeight: 55.0,
             floating: true,
             pinned: true,
-            title: Text(
-              '깔롱의 사진',
-              style: TextStyle(color: Colors.white),
-            ),
+            title: Text('깔롱의 사진', style: TextStyle(color: Colors.white)),
             centerTitle: true,
             elevation: 0,
-            leading: Text(''),
           ),
+          // Message
           SliverList(
             delegate: SliverChildListDelegate(
               [
@@ -288,7 +501,7 @@ class ChoicePictureState extends State<ChoicePicture> {
                     '내 사진을 골라주세요!',
                     style: TextStyle(
                       fontSize: 25,
-                      color: Color.fromARGB(255, 0, 0, 0),
+                      color: Colors.black,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
@@ -296,8 +509,9 @@ class ChoicePictureState extends State<ChoicePicture> {
               ],
             ),
           ),
+          // Grid of Clothes
           SliverPadding(
-            padding: const EdgeInsets.fromLTRB(20, 1, 20, 0),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
             sliver: SliverGrid(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
@@ -308,15 +522,26 @@ class ChoicePictureState extends State<ChoicePicture> {
                 (BuildContext context, int index) {
                   final item = savecloset['list']?[index];
                   if (item == null) {
-                    return const SizedBox(); // 빈 위젯 반환
+                    return const SizedBox.shrink();
                   }
                   return GestureDetector(
-                    onTap: () {},
+                    onTap: () {
+                      setState(() {
+                        selectedIndex = index; // Update the selected index
+                      });
+                    },
                     child: Container(
-                      color: const Color.fromARGB(255, 251, 235, 233),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 255, 244, 242),
+                        border: index == selectedIndex
+                            ? Border.all(
+                                color: const Color.fromARGB(255, 225, 154, 144),
+                                width: 3.0)
+                            : null,
+                      ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
+                        children: [
                           Image.asset(
                             item["image"] ?? "Assets/Image/logo.png",
                             height: 125,
@@ -340,8 +565,9 @@ class ChoicePictureState extends State<ChoicePicture> {
           ),
         ],
       ),
+      // Bottom Navigation Bar
       bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(20, 2, 20, 2),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -395,48 +621,50 @@ class ChoicePictureState extends State<ChoicePicture> {
             ),
             const SizedBox(height: 10),
             SizedBox(
-                width: 400,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const AvataPicture()),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFF5BEB5),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
+              width: 400,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AvataPicture()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF5BEB5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
                   ),
-                  child: const Text(
-                    '사진찍기 ',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                    ),
+                ),
+                child: const Text(
+                  '사진찍기',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
                   ),
-                )),
+                ),
+              ),
+            ),
             const SizedBox(height: 10),
             SizedBox(
-                width: 400,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFF5BEB5),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
+              width: 400,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFF5BEB5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
                   ),
-                  child: const Text(
-                    '내 패션 찰칵 ',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                    ),
+                ),
+                child: const Text(
+                  '내 패션 찰칵',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
                   ),
-                )),
+                ),
+              ),
+            ),
           ],
         ),
       ),
