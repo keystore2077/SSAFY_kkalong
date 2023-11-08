5ef95fd6fad59ae67716683b069d5e8aff4d91b6
dahae8 <dahae8@naver.com>
2023-11-08 14:45:02 +0900

    feet : 토큰 유효성 검사 기능 추가

--- backend/spring/kkalong/src/main/java/com/ssafy/kkalong/domain/cloth/repository/TagRelaionRepository.java
+++ backend/spring/kkalong/src/main/java/com/ssafy/kkalong/domain/cloth/repository/TagRelaionRepository.java
@@ -4,5 +4,8 @@ import com.ssafy.kkalong.domain.cloth.entity.TagRelation;
 import com.ssafy.kkalong.domain.cloth.entity.TagRelationKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRelaionRepository extends JpaRepository<TagRelation, TagRelationKey> {
+    List<TagRelation> findAllByClothClothSeq(int clothSeq);
 }
