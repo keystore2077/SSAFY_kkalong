272a47f9386a40581f20c2631967833d4545d88f
dahae8 <dahae8@naver.com>
2023-11-08 17:39:35 +0900

    feet : 옷 정보 수정 기능 추가

--- backend/spring/kkalong/src/main/java/com/ssafy/kkalong/domain/closet/repository/SectionRepository.java
+++ backend/spring/kkalong/src/main/java/com/ssafy/kkalong/domain/closet/repository/SectionRepository.java
@@ -4,8 +4,11 @@ import com.ssafy.kkalong.domain.closet.entity.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SectionRepository extends JpaRepository<Section, Integer> {
 
     List<Section> findAllByClosetClosetSeqAndIsSectionDeleted(int closetSeq, boolean isDeleted);
+
+    Optional<Section> findBySectionSeqAndIsSectionDeleted(int sectionSeq, boolean isDeleted);
 }
