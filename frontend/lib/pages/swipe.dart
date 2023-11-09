4b78150f90c89897839ed82fcd5a35bdf4ba59e4
김승영 <very7292@naver.com>
2023-11-10 01:00:32 +0900

    S09P31C105-366/feat/코디평가api

--- frontend/lib/pages/swipe.dart
+++ frontend/lib/pages/swipe.dart
@@ -1,46 +1,173 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
+import '../store/userstore.dart';
 
 class Swipe extends StatefulWidget {
-  const Swipe({Key? key}) : super(key: key);
+  const Swipe({
+    super.key,
+    this.storage,
+  });
+
+  final storage;
 
   @override
-  _MyAppState createState() => _MyAppState();
+  State<Swipe> createState() => SwipeState();
 }
 
-class _MyAppState extends State<Swipe> {
-  final TextEditingController inputController = TextEditingController();
-  Map<String, dynamic> saveavatar = {
-    'list': [], // list를 비워두거나 초기 데이터를 넣을 수 있습니다.
-  };
+class SwipeState extends State<Swipe> {
+  static final storage = FlutterSecureStorage();
+  String? accessToken;
 
+  @override
+  void initState() {
+    super.initState();
+    final userStore = Provider.of<UserStore>(context, listen: false);
+    accessToken = userStore.accessToken;
+    dioData(accessToken);
+  }
+
+  final TextEditingController inputController = TextEditingController();
+  final Dio dio = Dio(); // Dio HTTP 클라이언트 초기화
+  final serverURL = 'http://k9c105.p.ssafy.io:8761';
+
+  var data = [];
+  var imgUrl =
+      'https://mblogthumb-phinf.pstatic.net/MjAxODEyMTlfMTcz/MDAxNTQ1MjA0MTk4NDQy.-lCTSpFhyK1yb6_e8FaFoZwZmMb_-rRZ04AnFmNijB4g.ID8x5cmkX8obTOxG8yoq39JRURXvKBPjbxY_z5M90bkg.JPEG.cine_play/707211_1532672215.jpg?type=w800';
+  var fashionSeq = 0;
+  var fashionName = '';
+
+  Future<dynamic> dioData(token) async {
+    try {
+      final response = await dio.get('$serverURL/api/assess',
+          // queryParameters: {'userEmail': id}
+          options: Options(
+            headers: {
+              'Authorization': 'Bearer $token', // 토큰을 'Bearer' 스타일로 포함
+              // 다른 헤더도 필요한 경우 여기에 추가할 수 있습니다.
+            },
+          ));
+      var result = response.data['body'][0];
+      setState(() {
+        imgUrl = result['imgUrl'];
+        fashionSeq = result['fashionSeq'];
+        fashionName = result['fashionName'];
+      });
+      print(imgUrl);
+      return response.data;
+    } catch (e) {
+      print(e);
+      if (e is DioError) {
+        // DioError를 확인
+        _showErrorDialog('오류 발생: ${e.response?.statusCode}\n더이상 평가할 사진이 없습니다!');
+      } else {
+        _showErrorDialog('더이상 평가할 사진이 없습니다!');
+      }
+    }
+  }
+
+  Future<dynamic> likeData(token) async {
+    Response response;
+    try {
+      // final deviceToken = getMyDeviceToken();
+      final response = await dio.post('$serverURL/api/assess',
+          options: Options(
+            headers: {
+              'Authorization': 'Bearer $token', // 토큰을 'Bearer' 스타일로 포함
+              // 다른 헤더도 필요한 경우 여기에 추가할 수 있습니다.
+            },
+          ),
+          data: {
+            'fashionSeq': fashionSeq,
+            'like': true,
+          });
+      return response.data;
+    } catch (e) {
+      print(e);
+      if (e is DioError) {
+        // DioError를 확인
+        _showErrorDialog('오류 발생: ${e.response?.statusCode}\n더이상 평가할 사진이 없습니다!');
+      } else {
+        _showErrorDialog('더이상 평가할 사진이 없습니다!');
+      }
+    }
+  }
+
+  Future<dynamic> hateData(token) async {
+    Response response;
+    try {
+      // final deviceToken = getMyDeviceToken();
+      final response = await dio.post('$serverURL/api/assess',
+          options: Options(
+            headers: {
+              'Authorization': 'Bearer $token', // 토큰을 'Bearer' 스타일로 포함
+              // 다른 헤더도 필요한 경우 여기에 추가할 수 있습니다.
+            },
+          ),
+          data: {
+            'fashionSeq': fashionSeq,
+            'like': false,
+          });
+      return response.data;
+    } catch (e) {
+      print(e);
+      if (e is DioError) {
+        // DioError를 확인
+        _showErrorDialog('오류 발생: ${e.response?.statusCode}\n더이상 평가할 사진이 없습니다!');
+      } else {
+        _showErrorDialog('더이상 평가할 사진이 없습니다!');
+      }
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('오류 발생!'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('확인'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFFF5BEB5),
-        toolbarHeight: 55,
-        title: const Text(
-          '코디평가',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        elevation: 0,
-        leading: const Text(''),
-      ),
-      body: Container(
-          padding: const EdgeInsets.all(30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          const SliverAppBar(
+            backgroundColor: Color(0xFFF5BEB5),
+            expandedHeight: 55.0,
+            floating: true,
+            pinned: true,
+            title: Text(
+              '코디 평가',
+              style: TextStyle(color: Colors.white),
+            ),
+            centerTitle: true,
+            elevation: 0,
+            leading: Text(''),
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Container(
+              padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Padding(
                     padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
                     child: Text(
-                      '싸피깔롱쟁이',
+                      fashionName,
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.w600,
@@ -60,64 +187,78 @@ class _MyAppState extends State<Swipe> {
                   ),
                 ],
               ),
-              SwipeableCards(),
-              Center(
-                child: Image.asset('assets/swipe.png'),
-              ),
-            ],
-          )),
-    );
-  }
-}
-
-
-class SwipeableCards extends StatefulWidget {
-  @override
-  _SwipeableCardsState createState() => _SwipeableCardsState();
-}
-
-class _SwipeableCardsState extends State<SwipeableCards> {
-  List<String> cards = [
-    'https://image.newsis.com/2022/07/08/NISI20220708_0001037503_web.jpg',
-    'https://onlyyugioh.com/web/product/big/202205/5df59663bbd221d8148d3986fbe424c1.jpg',
-    'https://image.zdnet.co.kr/2023/07/26/f7981bfffc284d23d6335b1223bd554c.jpg',
-    'https://cdn.wwdkorea.com/news/photo/202305/4784_188109_5932.jpeg',
-    'https://blog.kakaocdn.net/dn/3ZrbN/btqCJlADYIx/Y56IertbI9cyiKnRMK4ys1/img.png',
-
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: cards.isEmpty
-          ? Text('No more cards')
-          : Dismissible(
-        key: Key(cards.first),
-        onDismissed: (direction) {
-          // 오른쪽에서 왼쪽으로 스와이프한 경우
-          if (direction == DismissDirection.endToStart) {
-            print('Swiped left'); // 원하는 로직으로 대체
-          }
-          // 왼쪽에서 오른쪽으로 스와이프한 경우
-          else if (direction == DismissDirection.startToEnd) {
-            print('Swiped right'); // 원하는 로직으로 대체
-          }
-
-          setState(() {
-            cards.removeAt(0);
-          });
-        },
-        child: Card(
-          child: Container(
-            height: 300,
-            width: 200,
-            alignment: Alignment.center,
-            child: Image.network(
-              cards.first,
-              fit: BoxFit.cover,
             ),
-          ),
-        ),
+          ])),
+          SliverPadding(
+              padding: const EdgeInsets.fromLTRB(2, 0, 2, 0),
+              sliver: SliverToBoxAdapter(
+                child: Container(
+                    padding: const EdgeInsets.fromLTRB(30, 25, 30, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          height: 300,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(
+                                Icons.chevron_left,
+                                size: 40,
+                                color: Color(0xFF54545b),
+                              ),
+                              Expanded(
+                                child: Center(
+                                  child: Dismissible(
+                                    key: Key(imgUrl),
+                                    onDismissed: (direction) {
+                                      final previousImgUrl = imgUrl;
+
+                                      // 오른쪽에서 왼쪽으로 스와이프한 경우 싫어요
+                                      if (direction ==
+                                          DismissDirection.endToStart) {
+                                        hateData(accessToken);
+                                      }
+                                      // 왼쪽에서 오른쪽으로 스와이프한 경우 좋아요
+                                      else if (direction ==
+                                          DismissDirection.startToEnd) {
+                                        likeData(accessToken);
+                                      }
+                                      dioData(accessToken);
+                                    },
+                                    child: Card(
+                                      child: Container(
+                                        height: 300,
+                                        width: 200,
+                                        alignment: Alignment.center,
+                                        child: Image.network(
+                                          imgUrl,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Icon(
+                                Icons.chevron_right,
+                                size: 40,
+                                color: Color(0xFF54545b),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                          child: Image.asset('Assets/Image/swipe.png'),
+                        ),
+                      ],
+                    )),
+              )),
+        ],
       ),
     );
   }
