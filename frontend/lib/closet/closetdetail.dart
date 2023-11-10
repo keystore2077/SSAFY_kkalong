775259ba5c41b876c18c654734e653cf2efa7690
김승영 <very7292@naver.com>
2023-11-10 17:59:28 +0900

    S09P31C105-371/feat/1110

--- frontend/lib/closet/closetdetail.dart
+++ frontend/lib/closet/closetdetail.dart
@@ -219,11 +219,16 @@ import 'package:flutter_mycloset/user/mypage.dart';
 import 'package:flutter_mycloset/user/nampage.dart';
 import '../closet/closetcloth.dart';
 import './clothcamera.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:provider/provider.dart';
+import '../store/userstore.dart';
 
 class ClosetDetail extends StatefulWidget {
-  const ClosetDetail({super.key, this.category});
+  const ClosetDetail({super.key, this.storage, required this.closetSeq});
 
-  final category;
+  final storage;
+  final closetSeq;
 
   @override
   State<ClosetDetail> createState() => _ClosetDetailState();
@@ -231,20 +236,84 @@ class ClosetDetail extends StatefulWidget {
 
 class _ClosetDetailState extends State<ClosetDetail> {
   final ScrollController scrollController = ScrollController();
+  static final storage = FlutterSecureStorage();
+  String? accessToken;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero, () async {
+      final userStore = Provider.of<UserStore>(context, listen: false);
+      final accessToken = userStore.accessToken;
+      dioData(accessToken);
+    });
+  }
+
+  final Dio dio = Dio(); // Dio HTTP 클라이언트 초기화
+  final serverURL = 'http://k9c105.p.ssafy.io:8761';
+
+  var data = {};
+  var sections = [];
+
+  Future<dynamic> dioData(token) async {
+    try {
+      final response = await dio.get('$serverURL/api/closet/${widget.closetSeq}',
+          // queryParameters: {'userEmail': id}
+          options: Options(
+            headers: {
+              'Authorization': 'Bearer $token', // 토큰을 'Bearer' 스타일로 포함
+              // 다른 헤더도 필요한 경우 여기에 추가할 수 있습니다.
+            },
+          ));
+      var result = response.data['body'];
+      setState(() {
+        data = result;
+        sections = result['closetSectionList'];
+      });
+      return response.data;
+    } catch (e) {
+      print(e);
+      if (e is DioError) {
+        // DioError를 확인
+        _showErrorDialog('오류 발생: ${e.response?.statusCode}');
+      } else {
+        _showErrorDialog('오류발생!');
+      }
+    }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('오류 발생!'),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: Text('확인'),
+            onPressed: () {
+              Navigator.of(ctx).pop();
+            },
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: DefaultTabController(
-          length: 4,
+          length: sections.length,
           child: NestedScrollView(
             controller: scrollController,
             headerSliverBuilder: (context, isScrolled) {
               return [
                 SliverAppBar(
-                  title: const Text(
-                    '씅쑤신다람쥐의 옷장',
+                  title: Text(
+                    data['closetName'],
                     style: TextStyle(color: Colors.white), // 텍스트의 색상을 흰색으로 설정
                   ),
                   centerTitle: true,
@@ -252,8 +321,8 @@ class _ClosetDetailState extends State<ClosetDetail> {
                   iconTheme: const IconThemeData(color: Colors.black),
                   collapsedHeight: 325,
                   expandedHeight: 325,
-                  flexibleSpace: Image.asset(
-                    'Assets/Image/ccloset.png',
+                  flexibleSpace: Image.network(
+                    data['closetPictureUrl'],
                   ),
                   actions: [
                     IconButton(
@@ -269,7 +338,7 @@ class _ClosetDetailState extends State<ClosetDetail> {
                   ],
                 ),
                 SliverPersistentHeader(
-                  delegate: MyDelegate(const TabBar(
+                  delegate: MyDelegate(TabBar(
                       labelColor: Colors.black,
                       indicatorWeight: 4,
                       unselectedLabelColor: Color(0xffD0D0D0),
@@ -278,25 +347,19 @@ class _ClosetDetailState extends State<ClosetDetail> {
                           TextStyle(fontWeight: FontWeight.w700, fontSize: 16),
                       indicatorColor: Color.fromARGB(255, 68, 25, 80),
                       indicatorSize: TabBarIndicatorSize.label,
-                      tabs: [
-                        Text('행거1'),
-                        Text('행거2'),
-                        Text('선반1'),
-                        Text('선반2'),
-                      ])),
+                      tabs: sections.map((sectionTitle) => Text(sectionTitle['sectionName'])).toList(),
+                  )),
                   floating: true,
                   pinned: true,
                 )
               ];
             },
-            body: const TabBarView(children: [
-              ClosetClothList(),
-              ClosetClothList(),
-              ClosetClothList(),
-              ClosetClothList(),
-            ]),
+            body: TabBarView(
+              children: sections.map((section) {
+                return ClosetClothList();
+              }).toList(),
           ),
-        ),
+        )),
 
         // floatingActionButton: FloatingActionButton.small(
         //   onPressed: () {
