7fbd9e59afc2dfd97a43bc8c0816ed1349e7728f
강신욱 <bearforce42@daum.net>
2023-11-15 16:32:42 +0900

    S09P31C105-388/feat/GPU서버에 있던 코드로 깃 업데이트

--- backend/fastapi/Preprocessing/RemBg/rb.py
+++ backend/fastapi/Preprocessing/RemBg/rb.py
@@ -3,17 +3,19 @@ from PIL import Image
 
 
 def remove_yes_bg(original_file_path: str, output_yes_bg: str):
-    original = Image.open(original_file_path)  # load image
-    output_image_white_bg = remove(original, alpha=False)  # 흰색 배경의 JPG 이미지
-    output_image_white_bg = output_image_white_bg.convert("RGB")  # RGBA를 RGB로 변환
-    output_image_white_bg.save(output_yes_bg, format='JPEG')
-    output_image_white_bg.close() #작업 끝나면 닫기
+    with Image.open(original_file_path) as img:
+        result = Image.new("RGB", img.size, "white")
+        out = remove(img)
+        result.paste(out, mask=out)
+        result.save(output_yes_bg, format='JPEG')
+        out.close()
 
 
 def remove_no_bg(original_file_path: str, output_no_bg: str):
     original = Image.open(original_file_path)  # load image
     output_image_transparent_bg = remove(original, alpha=True)  # 투명 배경의 PNG 이미지
     output_image_transparent_bg.save(output_no_bg, format='PNG')
+    print(output_no_bg)
     output_image_transparent_bg.close() #작업 끝나면 닫기
 
 
